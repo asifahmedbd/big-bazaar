815fde8cd0672f403a9ecfa07c7f7329e7cfd3d8
asifahmedbd <romeoasif@gmail.com>
2022-12-15 19:01:59 +0600

    shop category component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,19 +1,16 @@
-//import SHOP_DATA from '../../shop-data.json';
-import { UserContext } from "../../contexts/user.context";
-import { ProductContext } from "../../contexts/products.context";
-import { useContext } from "react";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Routes, Route } from 'react-router-dom';
 
 import './shop.styles.scss';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 const Shop = () => {
-    const {products} = useContext(ProductContext);
+    
     return (
-        <div className="products-container">
-            {products.map((product) =>(
-                <ProductCard key={product.id} product={product} />
-            ))}
-        </div>
-    )
-}
+        <Routes>
+            <Route index element={<CategoriesPreview />} />
+            <Route path=":category" element={<Category />} />
+        </Routes>
+    );
+};
 export default Shop;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -15,7 +15,11 @@ import {
   getFirestore,
   doc,
   getDoc,
-  setDoc
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs
 } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
@@ -46,12 +50,34 @@ const firebaseConfig = {
   //   experimentalAutoDetectLongPolling: true
   // });
 
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+      const docRef = doc(collectionRef, object.title.toLowerCase());
+      batch.set(docRef, object);
+    });
+    await batch.commit();
+  }
+
+  export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+      const {title, items} = docSnapshot.data();
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+    return categoryMap;
+  }
+
   export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     if(!userAuth) return;
     const userDocRef = doc(db, 'users', userAuth.uid);
-    console.log(userDocRef);
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot);
     
     if(!userSnapshot.exists()){
       const {displayName, email} = userAuth;
